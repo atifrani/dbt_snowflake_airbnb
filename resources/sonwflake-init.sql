9dda873e15a117353165c26ebe6d2af37ba6a779
atifrani <axeltifrani@gmail.com>
2023-11-09 15:24:34 +0100

    add kestra flow for snowflake

--- resources/sonwflake-init.sql
+++ resources/sonwflake-init.sql
@@ -9,6 +9,12 @@ GRANT ROLE TRANSFORM TO ROLE ACCOUNTADMIN;
 CREATE WAREHOUSE IF NOT EXISTS COMPUTE_WH;
 GRANT OPERATE ON WAREHOUSE COMPUTE_WH TO ROLE TRANSFORM;
 
+-- Create our database and schemas
+CREATE DATABASE IF NOT EXISTS AIRBNB;
+CREATE SCHEMA IF NOT EXISTS AIRBNB.BRONZE;
+CREATE SCHEMA IF NOT EXISTS AIRBNB.SILVER;
+CREATE SCHEMA IF NOT EXISTS AIRBNB.GOLD;
+
 -- Create the `dbt` user and assign to role
 CREATE USER IF NOT EXISTS dbt
   PASSWORD='dbtPassword123'
@@ -16,13 +22,10 @@ CREATE USER IF NOT EXISTS dbt
   MUST_CHANGE_PASSWORD=FALSE
   DEFAULT_WAREHOUSE='COMPUTE_WH'
   DEFAULT_ROLE='transform'
-  DEFAULT_NAMESPACE='AIRBNB.RAW'
+  DEFAULT_NAMESPACE='AIRBNB'
   COMMENT='DBT user used for data transformation';
 GRANT ROLE transform to USER dbt;
 
--- Create our database and schemas
-CREATE DATABASE IF NOT EXISTS AIRBNB;
-CREATE SCHEMA IF NOT EXISTS AIRBNB.RAW;
 
 -- Set up permissions to role `transform`
 GRANT ALL ON WAREHOUSE COMPUTE_WH TO ROLE transform; 
@@ -31,63 +34,7 @@ GRANT ALL ON ALL SCHEMAS IN DATABASE AIRBNB to ROLE transform;
 GRANT ALL ON FUTURE SCHEMAS IN DATABASE AIRBNB to ROLE transform;
 GRANT ALL ON ALL TABLES IN SCHEMA AIRBNB.RAW to ROLE transform;
 GRANT ALL ON FUTURE TABLES IN SCHEMA AIRBNB.RAW to ROLE transform;
-
-
-
--- Snowflake data import : Copy these SQL statements into a Snowflake Worksheet, select all and execute them.
-
--- Set up the defaults
-USE WAREHOUSE COMPUTE_WH;
-USE DATABASE airbnb;
-USE SCHEMA RAW;
-
--- Create our three tables and import the data from S3
-CREATE OR REPLACE TABLE raw_listings
-                    (id integer,
-                     listing_url string,
-                     name string,
-                     room_type string,
-                     minimum_nights integer,
-                     host_id integer,
-                     price string,
-                     created_at datetime,
-                     updated_at datetime);
-                    
-COPY INTO raw_listings (id,
-                        listing_url,
-                        name,
-                        room_type,
-                        minimum_nights,
-                        host_id,
-                        price,
-                        created_at,
-                        updated_at)
-                   from 's3://logbrain-datasets/airbnb/listings.csv'
-                    FILE_FORMAT = (type = 'CSV' skip_header = 1
-                    FIELD_OPTIONALLY_ENCLOSED_BY = '"');
-                    
-
-CREATE OR REPLACE TABLE raw_reviews
-                    (listing_id integer,
-                     date datetime,
-                     reviewer_name string,
-                     comments string,
-                     sentiment string);
-                    
-COPY INTO raw_reviews (listing_id, date, reviewer_name, comments, sentiment)
-                   from 's3://logbrain-datasets/airbnb/reviews.csv'
-                    FILE_FORMAT = (type = 'CSV' skip_header = 1
-                    FIELD_OPTIONALLY_ENCLOSED_BY = '"');
-                    
-
-CREATE OR REPLACE TABLE raw_hosts
-                    (id integer,
-                     name string,
-                     is_superhost string,
-                     created_at datetime,
-                     updated_at datetime);
-                    
-COPY INTO raw_hosts (id, name, is_superhost, created_at, updated_at)
-                   from 's3://logbrain-datasets/airbnb/hosts.csv'
-                    FILE_FORMAT = (type = 'CSV' skip_header = 1
-                    FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+GRANT ALL ON ALL TABLES IN SCHEMA AIRBNB.SILVER to ROLE transform;
+GRANT ALL ON FUTURE TABLES IN SCHEMA AIRBNB.SILVER to ROLE transform;
+GRANT ALL ON ALL TABLES IN SCHEMA AIRBNB.GOLD to ROLE transform;
+GRANT ALL ON FUTURE TABLES IN SCHEMA AIRBNB.GOLD to ROLE transform;
